6389e3309edddb6a679183ce2460a875596afb26
Manuel F. Espuch Sánchez <manuel-francisco.espuch@inetum.world>
2021-06-03 17:07:52 +0200

    Más avances

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../auth/AuthContext';
+import { types } from '../../types/types';
 
 export const LoginScreen = ({ history }) => {
 
+    const { dispatch } = useContext( AuthContext );
+
     const handleLogin = () => {
-        // history.push('/');
-        history.replace('/');
+        const lastPath = localStorage.getItem('lastPath') || '/';
+        dispatch({
+            type: types.login,
+            payload: {
+                name: 'Manuel'
+            }
+        });
+        history.replace(lastPath);
     };
 
     return (
